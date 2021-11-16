157296e8d4e91dd65521d7c96957822f0501ddc7
KrishanuS-1QBit <krishanu.sankar@1qbit.com>
2021-11-16 10:58:41 -0800

    adding minor functionality, creating a first database

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """Main file for modifying the database"""
 import pickle
-
+import argparse
 
 
 # We want the following functionality: given a year and a list of 1-3 authors, list all papers (handle + title) which
@@ -13,8 +13,10 @@ def add_new_entry(D):
     month = int(input('Type the reference month. '))
     arxiv = input('Type the arXiv identifier. ')
     authors = input("Type the list of author last names, separated by commas (no spaces). ").split(',')
+    tags = input("Add tags, separated by ' ; '.").split(' ; ')
     name = input("Type the reference handle, or type x if you would like one automatically generated. ")
-    name = f"{authors[0]}-{year}-{month}"
+    if name == 'x':
+        name = f"{authors[0]}-{year}-{month}"
 
     print("Type the handles for the important papers which this paper cites. Type 'done' when finished.")
     parents = []
@@ -41,6 +43,7 @@ def add_new_entry(D):
                'month': month,
                'arxiv': arxiv,
                'authors': authors,
+               'tags': tags,
                'parents': parents,
                'children': children}
 
@@ -72,47 +75,70 @@ def modify_entry(d):
 
     pass
 
-f = open('database.pckl', 'rb')
-D = pickle.load(f)
-f.close()
-print('Current list of papers:')
-for name in D.keys():
-    print(f"\nName: {name}")
-    entry = D[name]
-    for k in entry.keys():
-        print(f"{k}: {entry[k]}")
+def create_database(filename):
+    f = open(f"{filename}.pckl", 'wb')
+    pickle.dump(dict(), f)
+    f.close()
 
-while True:
-    print_str = "\nWhat would you like to do?"
-    print_str += "\nmk = add a paper"
-    print_str += "\nx = exit"
-    print_str += "\nrm = remove a paper"
-    print_str += "\nls = list the paper handles"
-    print_str += "\nopen XXX = get information about paper XXX"
-    print_str += "\nmod XXX = modify information about paper XXX"
-    X = input(print_str + "\n")
-    if X == 'x':
-        break
-    elif X == 'mk':
-        add_new_entry(D)
-    elif X == 'rm':
-        delete_entry(D)
-    elif X == 'ls':
-        for k in D.keys():
-            print(k)
-        print("")
-    elif X[:4] == 'open':
-        print("")
-        d = D[X[5:]]
-        for k in d.keys():
-            print(f"{k}: {d[k]}")
-        print("")
-    elif X[:3] == 'mod':
-        d = D[X[4:]]
-        modify_entry(d)
-    else:
-        print('Command not understood.')
+def main(args):
+    filename = args.filename
+    if args.new_database:
+        create_database(filename)
+    f = open(f"{filename}.pckl", 'rb')
+    D = pickle.load(f)
+    f.close()
 
-f = open('database.pckl', 'wb')
-pickle.dump(D, f)
-f.close()
+    print('Current list of papers:')
+    for name in D.keys():
+        print(f"\nName: {name}")
+        entry = D[name]
+        for k in entry.keys():
+            print(f"{k}: {entry[k]}")
+
+    while True:
+        print_str = "\nWhat would you like to do?"
+        print_str += "\nmk = add a paper"
+        print_str += "\nx = exit"
+        print_str += "\nrm = remove a paper"
+        print_str += "\nls = list the paper handles"
+        print_str += "\nopen XXX = get information about paper XXX"
+        print_str += "\nmod XXX = modify information about paper XXX"
+        X = input(print_str + "\n")
+        if X == 'x':
+            break
+        elif X == 'mk':
+            add_new_entry(D)
+        elif X == 'rm':
+            delete_entry(D)
+        elif X == 'ls':
+            for k in D.keys():
+                print(k)
+            print("")
+        elif X[:4] == 'open':
+            print("")
+            d = D[X[5:]]
+            for k in d.keys():
+                print(f"{k}: {d[k]}")
+            print("")
+        elif X[:3] == 'mod':
+            d = D[X[4:]]
+            modify_entry(d)
+        else:
+            print('Command not understood.')
+
+    f = open(f"{filename}.pckl", 'wb')
+    pickle.dump(D, f)
+    f.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--filename',
+        type=str, default='database'
+    )
+    parser.add_argument(
+        '--new_database',
+        type=bool, default=False
+    )
+    args = parser.parse_args()
+    main(args)
